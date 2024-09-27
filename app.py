8085d012aa14ebd615fc583c1e1658121d913e9f
giosanchez-iit <161739202+giosanchez-iit@users.noreply.github.com>
2024-09-27 22:45:51 +0800

    Homepage template completed

--- app.py
+++ app.py
@@ -1,22 +1,12 @@
 from flask import Flask, render_template
+import os
 
-app = Flask(__name__)
-
+app = Flask(__name__, 
+            template_folder='app/templates',
+            static_folder='app/static')
 @app.route('/')
 def home():
     return render_template('home.html')
-
-@app.route('/students')
-def students():
-    return render_template('students.html')
-
-@app.route('/programs')
-def courses():
-    return render_template('programs.html')
-
-@app.route('/colleges')
-def colleges():
-    return render_template('colleges.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
