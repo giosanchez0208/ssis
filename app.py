75e1f6e798d77522bbf99759502040e0ddd03585
giosanchez-iit <161739202+giosanchez-iit@users.noreply.github.com>
2024-09-29 09:12:56 +0800

    Added Students and Colleges Route to Flask

--- app.py
+++ app.py
@@ -4,9 +4,18 @@ import os
 app = Flask(__name__, 
             template_folder='app/templates',
             static_folder='app/static')
+
 @app.route('/')
 def home():
     return render_template('home.html')
+
+@app.route('/students')
+def students():
+    return render_template('students.html')
+
+@app.route('/colleges')
+def colleges():
+    return render_template('colleges.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
