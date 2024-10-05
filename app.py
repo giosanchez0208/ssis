8b740ab907bb4fdc435e800d4146dff81da92aed
giosanchez0208 <giokiefersanchez@gmail.com>
2024-10-05 17:58:24 +0800

    Add Student Bug Fixes

--- app.py
+++ app.py
@@ -21,23 +21,34 @@ with app.app_context():
 def home():
     return render_template('home.html')
 
+
 @app.route('/students', methods=['GET', 'POST'])
 def students():
     if request.method == 'POST':
         try:
-            id_num = f"{request.form['idPart1']}-{request.form['idPart2']}"
+            id_num = request.form['idNumber']
             first_name = request.form['firstName']
             last_name = request.form['lastName']
-            course = request.form.get('course', None)
+            course = request.form['course']
             year_level = request.form['year']
             gender = request.form['gender']
             
+            if not id_num or not first_name or not last_name or not year_level or not gender:
+                return render_template('students.html', 
+                                       students=StudentModel.query.all(),
+                                       programs=ProgramModel.query.all(),
+                                       error_message="Please fill out all required fields.")
+
             if gender == "Custom":
                 custom_gender = request.form.get('customGender', None)
                 if custom_gender:
                     gender = custom_gender
+                    
+            if course=="" or not course:
+                course = None
 
             new_student = StudentModel(
+                    
                 id_num=id_num,
                 first_name=first_name,
                 last_name=last_name,
@@ -47,22 +58,28 @@ def students():
             )
 
             db.session.add(new_student)
-            db.session.commit()
+            try:
+                db.session.commit()
+            except IntegrityError:
+                db.session.rollback()
+                return render_template('students.html', 
+                                       students=StudentModel.query.all(),
+                                       programs=ProgramModel.query.all(),
+                                       error_message="ID Number already exists.")
 
             return redirect('/students')
-        
-        except IntegrityError:
-            db.session.rollback() 
-            all_students = StudentModel.query.all()
-            all_programs = ProgramModel.query.all()
+
+        except Exception as e:
+            db.session.rollback()
             return render_template('students.html', 
-                                   students=all_students,
-                                   programs=all_programs,
-                                   error_message="ID Number already exists.")
+                                   students=StudentModel.query.all(),
+                                   programs=ProgramModel.query.all(),
+                                   error_message=f"An error occurred: {str(e)}")
 
     all_students = StudentModel.query.all()
     all_programs = ProgramModel.query.all()
     return render_template('students.html', students=all_students, programs=all_programs)
+
 
 @app.route('/check_id', methods=['POST'])
 def check_id():
